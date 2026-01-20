193562b7200afec67d24ea52b3cff5e2e2a97451
Kaushiki Agrawal <kaushikiagrawal27@gmail.com>
2026-01-19 23:27:56 -0800

    Initialized student model

--- src/main/java/com/example/Day3SMS/Model/StudentModel.java
+++ src/main/java/com/example/Day3SMS/Model/StudentModel.java
@@ -10,8 +10,7 @@ import java.util.Collection;
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
-@Documented
-@Document(collection="students")
+@Document(collection = "students")
 
 public class StudentModel {
     @Id
